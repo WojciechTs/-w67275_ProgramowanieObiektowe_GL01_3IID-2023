48253fe63956e51b22b4aab0851eb52f131f4739
WojciechTs <116261837+WojciechTs@users.noreply.github.com>
2024-01-01 22:28:54 +0100

    Lab 5

--- Lab 5/Zadanie3/Interface/IPersonRepository.cs
+++ Lab 5/Zadanie3/Interface/IPersonRepository.cs
@@ -7,7 +7,6 @@ namespace Lab5.Zadanie3.Interface
 {
     public interface IPersonRepository : IBaseRepository<Person, long>
     {
-        List<Book> GetBorrowedBook(long id);
-        void BorrowBook(long id, Book book);
+
     }
 }
